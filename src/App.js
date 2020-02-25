79a84e071286eb5d73b98cff5b3e5b0e82bef3ea
hanashiro <felipeaah@gmail.com>
2020-02-25 10:26:20 -0300

    aula-03-01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
-import React, {Component, useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
-
-class Counter extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      counter: 0
-    }
-  }
-
-  increment = () => {
-    this.setState(state => ({counter: state.counter + 1}));
-  }
-
-  render(){
-    const {state} = this;
-    return(
-      <button onClick={this.increment} >Classe: {state.counter}</button>
-    )
-  }
-}
 
 function Counter2(props){
   const [counter, setCounter] = useState(0);
@@ -32,7 +12,6 @@ function Counter2(props){
 function App() {
   return (
     <div className="App">
-      <Counter />
       <Counter2 />
     </div>
   );
