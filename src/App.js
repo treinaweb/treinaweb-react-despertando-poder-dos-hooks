7fc846a32fe1710b920f49c37dad301fabd829ab
hanashiro <felipeaah@gmail.com>
2020-02-25 10:07:35 -0300

    aula-02-01

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component, useState} from 'react';
 import './App.css';
+
+class Counter extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      counter: 0
+    }
+  }
+
+  increment = () => {
+    this.setState(state => ({counter: state.counter + 1}));
+  }
+
+  render(){
+    const {state} = this;
+    return(
+      <button onClick={this.increment} >Classe: {state.counter}</button>
+    )
+  }
+}
+
+function Counter2(props){
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <button onClick={()=> setCounter(counter + 1)} >Função: {counter}</button>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
+      <Counter2 />
     </div>
   );
 }
