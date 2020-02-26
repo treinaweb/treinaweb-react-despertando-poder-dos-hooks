2f3df934f6f4ab0073e7d9faefc3540160e58c2c
hanashiro <felipeaah@gmail.com>
2020-02-26 07:48:59 -0300

    aula-04-01

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, createRef} from 'react';
 import './App.css';
 
 function Counter2(props){
   const [counter, setCounter] = useState(0);
+  const button = createRef();
+
+  console.log(111, button)
+  useEffect(() => {
+    console.log(3333, button)
+  })
+
+  console.log(2222, button)
 
   return (
-    <button onClick={()=> setCounter(counter + 1)} >Função: {counter}</button>
+    <button ref={button} onClick={()=> setCounter(counter + 1)} >Função: {counter}</button>
   )
 }
 
